e626dc8dcb32bdec7210dea1bfa3a41ea184a30b
bpaksoy <paksoybanu83@gmail.com>
2017-12-13 01:34:53 -0500

    routing in progress

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,10 +1,17 @@
 import React from "react";
+import {Col, Card} from "react-materialize";
 
 const Profile =({name, userInfo})=>{
-  console.log("this is userInfo kihkihkih: ", userInfo);
+  console.log("this is userInfo:", userInfo);
   return(
    <div>
       <h1>Welcome to Your Profile {name}!!</h1>
+         <ul>
+           <li>{userInfo.profile.city}</li>
+           <li>{userInfo.profile.country}</li>
+           <li>Programs:</li>
+           {userInfo.programs.map((program, i)=><li key={i}>{program.program_name}</li>)}
+         </ul>
    </div>
   );
 }
